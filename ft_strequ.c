4edfed5343eb61384226555b7b632a84fd758c71
yodana <dana.yohann@gmail.com>
2018-11-21 09:11:06 +0000

    erreur a regler

--- ft_strequ.c
+++ ft_strequ.c
@@ -5,5 +5,11 @@ int ft_strnequ(char const *s1, char const *s2)
     int i;
 
     i = 0;
-    while (s[i])
+    while (s1[i] && s2[i])
+    {
+        if (s1[i] != s2[i])
+            return (0);
+        i++;
+    }
+    return (1);
 }
